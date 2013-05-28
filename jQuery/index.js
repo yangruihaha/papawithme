d706d0670831130b19efcd26f4e5cfa8c1b051f2
Yang Rui <ryang.pure@gmail.com>
2013-05-29 00:18:13 +0800

    haha

---
+++ jQuery/index.js
@@ -0,0 +1,64 @@
+// JavaScript Document
+$(document).ready(function() {
+    $('.login').bind("mouseover",function() {
+		$(this).css("opacity","1");
+        $('#login').css("opacity","1");
+		
+    }).bind("mouseout",function(){
+		$(this).css("opacity","0.4");
+        $('#login').css("opacity","0");
+	})
+
+});
+
+$(document).ready(function() {
+    $('.addFavorite').bind("mouseover",function() {
+		$(this).css("opacity","1");
+        $('#addFavorite').css("opacity","1");
+		
+    }).bind("mouseout",function(){
+		$(this).css("opacity","0.4");
+        $('#addFavorite').css("opacity","0");
+	})
+
+});
+
+$(document).ready(function() {
+    $('.bolg').bind("mouseover",function() {
+		$(this).css("opacity","1");
+        $('#bolg').css("opacity","1");
+		
+    }).bind("mouseout",function(){
+		$(this).css("opacity","0.4");
+        $('#bolg').css("opacity","0");
+	})
+
+});
+
+$(document).ready(function() {
+    $('.contactUs').bind("mouseover",function() {
+		$(this).css("opacity","1");
+        $('#contactUs').css("opacity","1");
+		
+    }).bind("mouseout",function(){
+		$(this).css("opacity","0.4");
+        $('#contactUs').css("opacity","0");
+	})
+
+});
+
+$(document).ready(function() {
+    $('.aboutUs').bind("mouseover",function() {
+		$(this).css("opacity","1");
+        $('#aboutUs').css("opacity","1");
+		
+    }).bind("mouseout",function(){
+		$(this).css("opacity","0.4");
+        $('#aboutUs').css("opacity","0");
+	})
+
+});
+
+
+
+
