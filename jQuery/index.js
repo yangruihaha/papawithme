efb1d4374409765c3a56863160f6b733673d960d
Rui Yang <ryang.pure@gmail.com>
2013-05-30 10:22:59 +0800

    柔化了图标透明度变换效果

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,60 +1,60 @@
 // JavaScript Document
 $(document).ready(function() {
     $('.login').bind("mouseover",function() {
-		$(this).css("opacity","1");
-        $('#login').css("opacity","1");
+		$(this).fadeTo("fast",1);
+        $('#login').fadeTo("fast",1);
 		
     }).bind("mouseout",function(){
-		$(this).css("opacity","0.4");
-        $('#login').css("opacity","0");
+		$(this).fadeTo('fast',0.4);
+        $('#login').fadeTo('fast',0.4);
 	})
 
 });
 
 $(document).ready(function() {
     $('.addFavorite').bind("mouseover",function() {
-		$(this).css("opacity","1");
-        $('#addFavorite').css("opacity","1");
+		$(this).fadeTo("fast",1);
+        $('#addFavorite').fadeTo("fast",1);
 		
     }).bind("mouseout",function(){
-		$(this).css("opacity","0.4");
-        $('#addFavorite').css("opacity","0");
+		$(this).fadeTo('fast',0.4);
+        $('#addFavorite').fadeTo('fast',0.4);
 	})
 
 });
 
 $(document).ready(function() {
     $('.blog').bind("mouseover",function() {
-		$(this).css("opacity","1");
-        $('#blog').css("opacity","1");
+		$(this).fadeTo("fast",1);
+        $('#blog').fadeTo("fast",1);
 		
     }).bind("mouseout",function(){
-		$(this).css("opacity","0.4");
-        $('#blog').css("opacity","0");
+		$(this).fadeTo('fast',0.4);
+        $('#blog').fadeTo('fast',0.4);
 	})
 
 });
 
 $(document).ready(function() {
     $('.contactUs').bind("mouseover",function() {
-		$(this).css("opacity","1");
-        $('#contactUs').css("opacity","1");
+		$(this).fadeTo("fast",1);
+        $('#contactUs').fadeTo("fast",1);
 		
     }).bind("mouseout",function(){
-		$(this).css("opacity","0.4");
-        $('#contactUs').css("opacity","0");
+		$(this).fadeTo('fast',0.4);
+        $('#contactUs').fadeTo('fast',0.4);
 	})
 
 });
 
 $(document).ready(function() {
     $('.aboutUs').bind("mouseover",function() {
-		$(this).css("opacity","1");
-        $('#aboutUs').css("opacity","1");
+		$(this).fadeTo("fast",1);
+        $('#aboutUs').fadeTo("fast",1);
 		
     }).bind("mouseout",function(){
-		$(this).css("opacity","0.4");
-        $('#aboutUs').css("opacity","0");
+		$(this).fadeTo('fast',0.4);
+        $('#aboutUs').fadeTo('fast',0.4);
 	})
 
 });
