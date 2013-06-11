44ed1463940aa3970f0883761e0cd8ef1ec06010
Yang Rui <ryang.pure@gmail.com>
2013-06-11 14:00:11 +0800

    intro

--- Lib/Action/IndexAction.class.php
+++ Lib/Action/IndexAction.class.php
@@ -1,11 +1,16 @@
 <?php
 // 本类由系统自动生成，仅供测试用途
 class IndexAction extends Action {
+	
     public function index(){
-		$this->assign('www','http://localhost/papawithme/');
 		$this->assign('public','http://localhost/papawithme/' . 'Public');
 		$this->display();
     }
+	
+	public function intro(){
+		$this->assign('public','http://localhost/papawithme/' . 'Public');
+		$this->display();
+	}
 	
 	public function login(){
 		$User = M('User'); 
